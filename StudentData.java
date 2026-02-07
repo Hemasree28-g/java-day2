0f9d9baee7d0d833f91ff87fd28edeeb55b1fe5b
hemasree <hemasree2827@gmail.com>
2026-02-07 12:28:12 +0530

    Add files via upload

---
+++ StudentData.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class StudentData{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int age=sc.nextInt();
+        String grade=sc.next();
+        int marks=sc.nextInt();
+        if (marks>=50) {
+            System.out.println("pass");
+        }else{
+            System.out.println("fale");
+        }
+    }
+}
