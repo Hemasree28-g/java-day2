1163b8b2c63b232745f4d2d20f09c22eef450df8
hemasree <hemasree2827@gmail.com>
2026-02-07 10:50:53 +0530

    Add files via upload

---
+++ Marksprimitive.java
@@ -0,0 +1,14 @@
+public class Marksprimitive{
+    public static void main(String[] args) {
+        int s1=50;
+        int s2=60;
+        int s3=70;
+        int total=s1+s2+s3;
+        int average=total/3;
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(total);
+        System.out.println(average);
+    }
+}
