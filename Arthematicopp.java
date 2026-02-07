b26c294ab9d6c9884f82368969478c8dbbb0e53a
hemasree <hemasree2827@gmail.com>
2026-02-07 13:54:15 +0530

    Add files via upload

---
+++ Arthematicopp.java
@@ -0,0 +1,11 @@
+public class Arthematicopp{
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        int sum=a+b;
+        System.out.println(sum);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
