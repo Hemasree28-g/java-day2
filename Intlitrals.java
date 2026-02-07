b6e930b06cfd1991917a715c6340fcb07a292edc
hemasree <hemasree2827@gmail.com>
2026-02-07 10:40:14 +0530

    Add files via upload

---
+++ Intlitrals.java
@@ -0,0 +1,15 @@
+public class Intlitrals
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=27;
+        int c=214;
+        int d=0110;
+        int e=0x5f2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
